b6f9363d92c17927f20aa935bc96afac29db7e2f
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-15 12:09:00 -0600

    refactored controllers folder

---
+++ Main/models/Thought.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+  {
+      reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+          type: String,
+          required: true,
+          maxLength: 280
+      },
+      username: {
+          type: String,
+          required: true
+      },
+      createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (time) => format_date(time)
+      }
+  },
+  {
+      toJSON: {
+          getters: true
+      },
+      id: false
+  }
+)
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'You need to leave a thought!',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
