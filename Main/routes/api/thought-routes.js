2cd3713a7dc8e4cf361971693bfc1bd7ac689ada
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-15 16:21:35 -0600

    adjusted thought-routes.js

--- Main/routes/api/thought-routes.js
+++ Main/routes/api/thought-routes.js
@@ -1,24 +1,31 @@
 const router = require('express').Router();
 const {
-  getThoughts,
+  getAllThoughts,
   getSingleThought,
   createThought,
   updateThought,
   deleteThought,
-  addReaction,
-  removeReaction,
+  createReaction,
+  deleteReaction,
 } = require('../../controllers/thought-controller');
 
-// /api/thoughts
-router.route('/').get(getThoughts).post(createThought);
+router
+.route('/')
+.get(getAllThoughts)
+.post(createThought)
 
-// /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router
+.route('/:id')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought)
 
-// /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction);
+router
+.route('/:thoughtId/reactions')
+.post(createReaction)
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction)
 
-module.exports = router;
+module.exports = router
